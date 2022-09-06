7cb03f822b590f2f299d8649e70cd5eadad907fc
Madhav Bhalani <madhavbhalani@gmail.com>
2022-09-06 13:59:15 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,9 @@
+document.write(`
+<footer>
+  <div class="footheading">Madhav Bhalani</div>
+  <div class="footitm">
+    <i class="bx bx-copyright" style="background-color: #f2e5d7"></i> Copyright
+    2022, Madhav Bhalani. All Rights Reserved F
+  </div>
+</footer>
+`)
---
+++ header.js
@@ -0,0 +1,27 @@
+document.write(`
+<header>
+  <span class="heading">MADHAV BHALANI</span>
+  <nav class="nav_links">
+    <ul>
+      <li id="home_link"><a href="index.html">Home</a></li>
+      <li id="about_link"><a href="about.html">About</a></li>
+      <li id="skills_link"><a href="skills.html">Skills</a></li>
+    </ul>
+  </nav>
+  <a href="tel:8200158880"><button>Contact Me</button></a>
+</header>
+`)
+
+console.log(location.pathname)
+if(location.pathname === "/index.html"){
+  let activeLink = document.getElementById("home_link")
+  activeLink.className = "nav_links active"
+}
+else if(location.pathname === "/about.html"){
+  let activeLink = document.getElementById("about_link")
+  activeLink.className = "nav_links active"
+}
+else if(location.pathname === "/skills.html"){
+  let activeLink = document.getElementById("skills_link")
+  activeLink.className = "nav_links active"
+}
